6005c81f4188fbc7a0dc025505cc6e5f7e1d386f
yannicowie <yannicowie@hotmail.com>
2019-02-09 23:07:02 +1300

    Update LocalMonitor.ps1
    Alpha version. Now reports the names of any TCP/IP printers whose ports appear to be down

--- LocalMonitor.ps1
+++ LocalMonitor.ps1
@@ -1,5 +1,10 @@
 ####PowerShell Script that tests the connection to all TCP/IP print server ports and then reports on those that are not available.
 
+#Initialising the display list output
+Write-Host "This script may take some time to complete as it needs to check each port"
+pause
+clear-host
+
 #Getting lists of all printers and all TCP/IP ports on the computer
 $allTCPIPports = Get-WMIObject win32_tcpipprinterport
 
@@ -10,6 +15,7 @@ $numberOfPrinters = $allPrinters.length
 
 [array]$PortsInUseArray = @()
 [array]$PrintersInUseArray = @()
+[array]$DeadPrinters = @()
 
 #function to carry out a test TCP connection to the printer port
 function Port-Tester {
@@ -24,15 +30,31 @@ function Port-Tester {
 		
 		$conntest = test-netconnection $hostname -Port $portnum
 		
-		if ($conntest.TcpTestSucceeded -eq "False"){
-			#the port is down, so we need to log the name of the printer and present that as the result of the script
-			write-host "Printer down: " $PrintersInUseArray[$testerCounter].name -ForegroundColor Red
-		}
+		$conntestresult = $conntest.TcpTestSucceeded
 		
+		if ($conntestresult = "False"){
+			#the port is down, so we need to log the name of the printer and present that as the result of the script
+			$printerdown = $PrintersInUseArray[$testerCounter].name
+			$DeadPrinters = $DeadPrinters + $printerdown
+			
+			#now that we've notified the user the printer is down, we can move on and check the next printer in the list if there is one
+			$testerCounter = $testerCounter + 1
+			Port-Tester
+		}
+		else {
+			#that was a good port, so increase the counter by one and trigger the function again
+			$testerCounter = $testerCounter + 1
+			Port-Tester
+		}
 	}
 	
 	else {
 		#we must have tested all the ports, so time to finish the function
+		clear-host
+		write-host "List of printers that are dead:"
+		write-host ""
+		write-host $DeadPrinters -foregroundcolor red
+		write-host ""
 		write-host "The script has finished!"
 	}
 	
