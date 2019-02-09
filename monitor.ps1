d72bc1b81d25b35fd7079866989a93b932f12730
yannicowie <yannicowie@hotmail.com>
2019-02-09 17:52:29 +1300

    Update monitor.ps1

--- monitor.ps1
+++ monitor.ps1
@@ -10,6 +10,8 @@ $portCount = $allPorts.length
 $global:DeadPorts = $false
 
 function Port-Tester{
+
+	#Doing a TCP connection test to the port if the number of ports to count is less than the number that have already been counted.
 	if ($counter -lt $portCount){
 	
 		$IPAddress = $allPorts[$counter].HostAddress
@@ -21,6 +23,7 @@ function Port-Tester{
 		$ComputerName = $env:computername
 		$PortName = $allPorts[$counter].Name
 		
+		#If the TCP connection test to the port fails, writing this to the log file
 		if ($PortLive = "False"){
 		
 			$logline = "On " + $ComputerName + " Port Name " + $PortName + " is down. Trying to connect to " + $IPAddress + " -port " + $PortNumber
@@ -29,21 +32,24 @@ function Port-Tester{
 			$global:DeadPorts = $true
 			
 		}
-			
+		
+		#Adding +1 to the $counter
 		$counter = $counter + 1
 		#Triggering the function again to run the test on the next port
 		Port-Tester
 	}
 	
+	#If all ports have been tested, announce completion
 	else {
 	
 		write-host "Port test has finished"
 		
-		if ($global:DeadPorts = $true){
-			write-host "There are some dead ports!"
-		}
+		#Announcing if there were any TCP ports that a connection couldn't be established to.
+		if ($global:DeadPorts = $true){write-host "There are some dead ports!"}
 	}
 }
 
+#Initialising the $counter variable and setting it to 0 (i.e. no ports counted yet)
 $counter = 0
+#Triggering the port testing function
 Port-Tester
